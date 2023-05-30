f64a9928aba98c75ff9cb0300594eac3ef88c08c
Kamil <mentos.96@o2.pl>
2023-05-30 22:06:35 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+class TallGuy
+{
+    public string Name;
+    public int Height;
+    
+
+    public void TalkAboutYourlself()
+    {
+
+    }
+
+}
